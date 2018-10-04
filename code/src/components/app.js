cba7ea5c418d0cd99077e0d5b589096bf0529077
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2018-10-04 11:03:30 +0200

    Fetched data

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,11 +1,28 @@
 import React from "react"
+import Station from "./station/station.js"
+
 
 class App extends React.Component {
 
+    state = {
+      stationItems: []
+    }
+
+  componentDidMount() {
+    fetch("http://api.sr.se/api/v2/channels?format=json&size=100").then((response) => {
+      return response.json()
+    }).then((json) => {
+      this.setState({
+        stationItems: json.channels
+      })
+    })
+  }
+
   render() {
+
     return (
-      <div>
-        Find me in src/app.js!
+      <div className="App">
+        <Station />
       </div>
     )
   }
